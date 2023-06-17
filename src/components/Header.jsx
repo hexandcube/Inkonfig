a2afbd8f684785df825d7f75a89cafebed86be0e
Hexandcube <hexandcube@hexandcube.eu.org>
2023-06-17 12:51:32 +0200

    Minor tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,18 @@ export default function header() {
           <div className=" text-center">
             <img src="/assets/logo-full-white.png" alt="Inkonfig" width="20%"/>
             <p className="mt-2 title">
-              A quick way to configure your Windows device
+              Windows configuration script generator
             </p>
             <p className="is-family-monospace">
               Version {packageJson.version} | Supported: Windows 11, Windows 10
             </p>
             <div className="mt-5">
-              <a href="https://inkonfigdocs.hexandcube.eu.org" className="button is-link is-light mr-2">
+              <a href="https://inkonfigdocs.hexandcube.eu.org" className="button is-primary is-light mr-2">
                 Documentation
               </a>
-              {/* <a href="https://inkonfigdocs.hexandcube.eu.org" className="button is-warning is-light ml-2">
-                Learn more
-              </a> */}
+              <a href="https://github.com/hexandcube/Inkonfig/" className="button is-link is-light ml-2">
+                Source Code
+              </a> 
             </div>
           </div>
         </div>
