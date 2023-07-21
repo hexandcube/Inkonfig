33ae8c782b0255dd80006fb49e779b3673a3d5af
Hexandcube <hexandcube@hexandcube.eu.org>
2023-07-21 12:20:14 +0200

    Fix #79

--- src/scripts/inkonfigGenerate.js
+++ src/scripts/inkonfigGenerate.js
@@ -187,7 +187,7 @@ echo [Inkonfig] Configuring your settings, please wait...`;
     
   // Software
 
-  if (formData.installSoftware) {
+  if (formData.installSoftwareChoco) {
     if (!formData.dontInstallChocolatey) {
       scriptContent += `\n@"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin"`;
     }
